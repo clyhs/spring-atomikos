2772599254d47591cb8caee45a0a8aca408839ba
clyhs <clygd@126.com>
2016-06-06 12:17:19 +0800

    redis

---
+++ spring-atomikos/sa-redis/src/main/java/org/sa/redis/JedisDataSourceImpl.java
@@ -0,0 +1,39 @@
+package org.sa.redis;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import redis.clients.jedis.ShardedJedis;
+import redis.clients.jedis.ShardedJedisPool;
+
+@Repository("jedisDS")
+public class JedisDataSourceImpl implements JedisDataSource {
+    private static final Logger LOG = LoggerFactory.getLogger(JedisDataSourceImpl.class);
+    
+    @Autowired
+    private ShardedJedisPool shardedJedisPool;
+    
+    public ShardedJedis getRedisClient() {
+        ShardedJedis shardJedis = null;
+        try {
+            shardJedis = shardedJedisPool.getResource();
+            return shardJedis;
+        } catch (Exception e) {
+            LOG.error("[JedisDS] getRedisClent error:" + e.getMessage());
+            if (null != shardJedis)
+                shardJedis.close();
+        }
+        return null;
+    }
+
+    public void returnResource(ShardedJedis shardedJedis) {
+        shardedJedis.close();
+    }
+
+    public void returnResource(ShardedJedis shardedJedis, boolean broken) {
+        shardedJedis.close();
+    }
+
+}
